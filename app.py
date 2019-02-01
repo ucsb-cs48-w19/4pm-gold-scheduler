03393d9d3f03222b6b8e5c00fb4ae4fcf938e361
Yuchen Bi <yuchenbi@csil-41.cs.ucsb.edu>
2019-01-31 22:39:51 -0800

    initial project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+if __name__ == '__main__':
+    app.debug=True
+    port=int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
+
+
+
