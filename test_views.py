c5fc5689a786cb58eb1c911eb1944f92cdb2169b
YanwuTong <35502088+YanwuTong@users.noreply.github.com>
2019-02-28 10:49:33 -0800

    Update test_views.py

--- test_views.py
+++ test_views.py
@@ -5,9 +5,16 @@ from flask import render_template, Flask, request, jsonify, session, redirect, u
 from views import index, dropDownPageNew
 from app import app
 
-
-
+# ensure that flask was set up the index page correctly
 def test_views_index():
 	tester = app.test_client()
 	response = tester.get('/', content_type = 'html/text')
 	assert response.status_code == 200
+
+
+#ensure that flask was set up the course selection page correctly
+def test_views_dropDownPageNew():
+	tester = app.test_client()
+	response = tester.get('/class-select', content_type = 'html/text')
+	assert response.status_code == 200
+
