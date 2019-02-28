e3193047841539fffea05eefaa20b094ede083db
YanwuTong <35502088+YanwuTong@users.noreply.github.com>
2019-02-28 09:37:45 -0800

    Delete test_views.py

--- test_views.py
+++
@@ -1,9 +0,0 @@
-import os 
-import tempfile
-import json
-from flask import Flask, render_template, request
-from views import index
-from app import app
-
-def test_index():
-  assert index() == render_template('index.html')
