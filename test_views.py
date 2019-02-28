c65f2aa2a3f71b0850995669dc575a855aca8e83
YanwuTong <35502088+YanwuTong@users.noreply.github.com>
2019-02-28 11:03:01 -0800

    Update test_views.py

--- test_views.py
+++ test_views.py
@@ -5,16 +5,27 @@ from flask import render_template, Flask, request, jsonify, session, redirect, u
 from views import index, dropDownPageNew
 from app import app
 
-# ensure that flask was set up the index page correctly
-def test_views_index():
-	tester = app.test_client()
-	response = tester.get('/', content_type = 'html/text')
-	assert response.status_code == 200
+class FlaskTestCase(unittest.TestCase):
+
+	# ensure that flask was set up the index page correctly
+	def test_views_index(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertEqual(response.status_code,200)
+
+	# ensure that the index page loads correctly
+	def test_views_index_load(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertTrue(b'Gold Scheduler' in response.data)
 
 
-#ensure that flask was set up the course selection page correctly
-def test_views_dropDownPageNew():
-	tester = app.test_client()
-	response = tester.get('/class-select', content_type = 'html/text')
-	assert response.status_code == 200
+	#ensure that flask was set up the course selection page correctly
+	def test_views_dropDownPageNew(self):
+		tester = app.test_client(self)
+		response = tester.get('/class-select', content_type = 'html/text')
+		self.assertEqual(response.status_code,200)
+
+if __name__ == '__main__':
+	unittest.main()
 
