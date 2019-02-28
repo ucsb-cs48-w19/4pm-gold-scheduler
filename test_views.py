49770e914573a113ecef024d2c54f121164b87e7
YanwuTong <35502088+YanwuTong@users.noreply.github.com>
2019-02-28 09:26:31 -0800

    Update test_views.py

--- test_views.py
+++ test_views.py
@@ -2,7 +2,7 @@ import os
 import tempfile
 from flask import render_template
 from views import index
-from app improt app
+from app import app
 
 def test_index():
   assert index() == render_template('index.html')
