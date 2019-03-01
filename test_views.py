87cf5945cb9df6f9846d24c8e3992768abfd0d77
y_tong@umail.ucsb.edu <y_tong@umail.ucsb.edu>
2019-03-01 13:44:43 -0800

    second test

---
+++ test_views.py
@@ -0,0 +1,38 @@
+import pytest
+import unittest
+
+from flask import render_template, Flask, request, jsonify, session, redirect, url_for, flash
+from routes import index, dropDownPageNew
+from app import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+	# ensure that flask was set up the index page correctly
+	def test_views_index(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertEqual(response.status_code,200)
+
+	# ensure that the index page loads correctly
+	def test_views_index_load(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertTrue(b'Gold Scheduler' in response.data)
+
+
+	#ensure that flask was set up the course selection page correctly
+	def test_views_dropDownPageNew(self):
+		tester = app.test_client(self)
+		response = tester.get('/class-select', content_type = 'html/text')
+		self.assertEqual(response.status_code,200)
+
+	def test_views_dropDownPageNew_load(self):
+		tester = app.test_client(self)
+		response = tester.get('/', content_type = 'html/text')
+		self.assertTrue(b'Gold Scheduler' in response.data)
+
+
+if __name__ == '__main__':
+	unittest.main()
+
